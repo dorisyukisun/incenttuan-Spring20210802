a9fad4d3dda958102d820d419f5d11d8c0d60e50
vincenttuan <vincentjava@yahoo.com.tw>
2021-08-06 19:39:43 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -0,0 +1,16 @@
+package com.study.spring.case01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Author author1 = ctx.getBean(Author.class);
+		Author author2 = (Author)ctx.getBean("author");
+		System.out.println(author1);
+		System.out.println(author2);
+	}
+
+}
