c77e5490f63a9331715832dc75ffe29aefd5b9ff
vincenttuan <vincentjava@yahoo.com.tw>
2021-08-06 21:13:29 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case01/MainTest2.java
@@ -0,0 +1,27 @@
+package com.study.spring.case01;
+
+/*
+public class Husband {
+    private String name;
+    private Wife wife;
+    //...
+}
+
+public class Wife {
+    private String name;
+    private Husband husband;
+    //...
+}
+用 Spring 配置
+John 的老婆是 Mary
+Mary 的丈夫是 John
+*/
+
+public class MainTest2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
